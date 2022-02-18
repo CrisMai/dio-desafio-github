aed7a6be8089ce092d4d0f1850cf5d64f4140abc
Cris Mai <crismai.dev@gmail.com>
2022-02-18 16:05:50 -0300

    POO - polimorfismo

--- banco-digital/src/Conta.java
+++ banco-digital/src/Conta.java
@@ -1,9 +1,19 @@
 
 public abstract class Conta implements IConta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
+    private static final int AGENCIA_PADRAO = 1;
+
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     @Override
     public void sacar(double valor) {
@@ -32,7 +42,6 @@ public abstract class Conta implements IConta {
     public double getSaldo() {
         return saldo;
     }
-
 
 
 }
--- banco-digital/src/ContaPoupanca.java
+++ banco-digital/src/ContaPoupanca.java
@@ -1,5 +1,4 @@
 
 public class ContaPoupanca extends Conta {
 
-
 }
